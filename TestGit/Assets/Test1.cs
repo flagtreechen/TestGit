0d1486458a1730b414238ca190c2adcd873bfcbf
treechen <treechen@fla.com.tw>
2020-05-29 14:53:46 +0800

    t2-1

--- TestGit/Assets/Test1.cs
+++ TestGit/Assets/Test1.cs
@@ -7,9 +7,11 @@ public class Test1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int aa = 0;
+        int aa = 2;
         int bb = 1;
-        int cc = 2;
+        int cc = 0;
+        int dd = 3;
+        int ee = 4;
     }
 
 
